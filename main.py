78be8b01c58d92c552a183aaad99354bdaa84881
@LucasMtss <lmatos@f3capital.com.br>
2022-08-19 11:36:03 -0300

    feat: add integration with database

--- main.py
+++ main.py
@@ -1,31 +1,164 @@
+from pyexpat.errors import messages
+from timeit import repeat
+from sqlalchemy import null, true
+from unidecode import unidecode
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+import numpy
+from dateutil.parser import *
 import os
-
-import flask
-from flask import send_from_directory, request
-
-app = flask.Flask(__name__)
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/favicon.png')
-
-@app.route('/')
-@app.route('/home')
-def home():
-    return "Hello World"
-
+from dotenv import load_dotenv
 from sendMessage import sendMessage
+from datetime import datetime
+
+# MENSAGENS
+INITIAL_MESSAGE = """Olá, em que posso ajudá-lo?
+[1] Verificar lembretes de hoje
+[2] Verificar lembretes de outro dia
+[3] Adicionar lembrete
+"""
+ERROR_MESSAGE = 'Desculpe, não consegui entender o que você digitou :('
+CREATE_REMINDER_ERROR_MESSAGE = 'Desculpe, não foi possível criar o lembrete :('
+CREATE_REMINDER_SUCCESS_MESSAGE = 'Lembrete criado com sucesso!!'
+SEPARATOR='\n--------------------------------------------------------------------\n\n'
+INFORM_THE_DATE='Informe a data: (dd/mm/aaaa)'
+CREATE_REMINDER_SAMPLE="""Por favor, informe seu lembrete no seguinte formato:
+
+Dia: dd/mm/aaaa
+Hora: HH:mm
+Lembrete: 'Lembrar de...'
+"""
+
+load_dotenv()
+
+dbName=os.environ.get('DB_NAME')
+dbHost=os.environ.get('DB_HOST')
+dbUser=os.environ.get('DB_USER')
+dbPassword=os.environ.get('DB_PASSWORD')
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{dbUser}:{dbPassword}@{dbHost}/{dbName}'
+
+
+db = SQLAlchemy(app)
+
+class Lembrete(db.Model):
+    id = db.Column(db.Integer, primary_key=true)
+    date = db.Column(db.String(10), nullable=False)
+    hour = db.Column(db.String(5), nullable=False)
+    notes = db.Column(db.String(100))
+    active = db.Column(db.Boolean(), nullable=False)
+    repeat = db.Column(db.String(15))
+
+    def toJson(self):
+        return {
+            "id": self.id,
+            "date": self.date,
+            "hour": self.hour,
+            "notes": self.notes,
+            "active": self.active,
+            "repeat": self.repeat,
+        }
+
+def formatRemindersForMessage(reminders, date):
+    if(numpy.size(reminders) == 0):
+        return f'Você não tem lembretes para o dia {date}'
+
+    message = f"LEMBRETES DO DIA {date}\n\n"
+    for reminder in reminders:
+        message = message + f"{reminder['hour']}h"
+        message = message + ' - '
+        message = message + reminder['notes'] 
+        message = message + SEPARATOR
+    return message
+
+def validateDate(message):
+    message = message.replace("/","-",2)
+    res = True
+    try:
+        res = bool(parser().parse(message))
+    except ValueError:
+        res = False
+    return res
+
+def formatDate(date):
+    formatedDate = date.split('-')
+    return f'{formatedDate[2]}/{formatedDate[1]}/{formatedDate[0]}'
+
+def findRemindersOfToday():
+    today = f'{datetime.today()}'
+    today = today[:10]
+    return findReminders(formatDate(today))
+
+def findReminders(date):
+    reminders = Lembrete.query.filter_by(date=date)
+    response = [reminder.toJson() for reminder in reminders]
+    return formatRemindersForMessage(response, date)
+
+def formatReminderToCreate(message):
+    splitMessage = message.split("\n")
+    newReminder = null
+    newReminder.date = splitMessage[0]
+    newReminder.hour = splitMessage[1]
+    newReminder.notes = splitMessage[2]
+    newReminder.active = True
+    newReminder.repeat = ''
+    return newReminder
+
+def createReminder(reminder):
+    try:
+        reminder = Lembrete(
+            date=reminder.date,
+            hour=reminder.hour,
+            notes=reminder.notes,
+            active=reminder.active,
+            repeat=reminder.repeat)
+        db.session.add(reminder)
+        db.session.commit()
+        return CREATE_REMINDER_SUCCESS_MESSAGE
+    except Exception as error:
+        print('ERROR', error)
+        return CREATE_REMINDER_ERROR_MESSAGE
+
+def getMessage(request):
+    return request.form['Body']
+
+def handleUserMessage(message):
+    formatedMessage = unidecode(message.lower())
+    if formatedMessage == 'ola':
+        return INITIAL_MESSAGE
+    if formatedMessage == '1':
+        return findRemindersOfToday()
+    if formatedMessage == '2':
+        return INFORM_THE_DATE
+    if formatedMessage == '3':
+        return CREATE_REMINDER_SAMPLE
+    if checkIfNewReminder(formatedMessage) == True:
+        reminder = formatReminderToCreate(message)
+        response = createReminder(reminder)
+        return response
+    if validateDate(message) == True:
+        res = findReminders(message)
+        return res
+    else:
+        return ERROR_MESSAGE
+
+def checkIfNewReminder(message):
+    if numpy.size(message.split("\n")) == 3:
+        return True
+    else:
+        return False
 
 @app.route('/whatsapp', methods=['GET', 'POST'])
 def whatsapp():
-    print(request.get_data())
-    message = request.form['Body']
-    senderId = request.form['From'].split('+')[1]
-    print(f'Message --> {message}')
-    print(f'Sender id --> {senderId}')
-    res = sendMessage(senderId=senderId, message=message)
-    print(f'This is the response --> {res}')
+    response = handleUserMessage(getMessage(request))
+    if(response != ERROR_MESSAGE):
+        senderId = request.form['From'].split('+')[1]
+        sendMessage(senderId=senderId, message=response)
+    else:
+        sendMessage(senderId=senderId, message=ERROR_MESSAGE)
+        sendMessage(senderId=senderId, message=INITIAL_MESSAGE)
     return '200'
 
 if __name__ == "__main__":
